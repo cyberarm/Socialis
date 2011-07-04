5aa430f82d995f3987c66a7cdf671adc46ff426e
Cyber <matthewlikesrobots@gmail.com>
2011-07-04 14:03:57 -0500

    updates/fixes for version 0.0.0.70

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -4,9 +4,9 @@ before_filter :login_required, :except => [:index, :show]
   # List all the statuses users have made
   def index
     @userl = User.find_by_username(params[:id]) || User.find_by_id(params[:id])
-    @status = Status.order('created_at').page(params[:page]).per(5)
+    @status = Status.order('created_at').page(params[:page]).per(10)
     #@statusdel = Status.find_by_shortened(params[:id])
-    @status_paginate = Status.order('created_at').page(params[:page]).per(5)
+    @status_paginate = Status.order('created_at').page(params[:page]).per(10)
     respond_to do |format|
   format.html
   format.json { render :json => @status }
@@ -30,10 +30,10 @@ def create
     @status = Status.create(:message => params[:message], :author => params[:author], :email => params[:email])  
     respond_to do |format|  
       if @status.save  
-	      flash[:notice] = "Status Saved"
+	      flash[:notice] = "Status created successfully."
         format.html { redirect_to "/statuses" }  
       else  
-        flash[:notice] = "Status failed to save."  
+        flash[:notice] = "Status failed to save. Please try again soon."  
         format.html { redirect_to "/status/new" }  
       end  
     end  
@@ -43,7 +43,7 @@ def create
   def destroy
     @status = Status.find(params[:id])
     if @status.destroy
-      flash[:notice] = "Deleted Status"
+      flash[:notice] = "Status deleted."
     else
       flash[:error] = "Status did not delete. Try again later."
     end
